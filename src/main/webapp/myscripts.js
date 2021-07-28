153293b804da816d372566d214fef8848ba154e7
gugushabeso <ozashvili1@gmail.com>
2021-07-28 23:11:48 +0400

    implement request functions

--- src/main/webapp/myscripts.js
+++ src/main/webapp/myscripts.js
@@ -49,6 +49,29 @@ function addQuestion(btn) {
 		}
 	}
 
+	function requestFunc() {
+		var xhttp = new XMLHttpRequest();
+		xhttp.onreadystatechange = function() {
+			if (xhttp.readyState == 4 && xhttp.status == 200) {
+				document.getElementById("content").innerHTML = xhttp.responseText;
+			}
+		};
+		xhttp.open("GET", "FriendRequests", true)
+		xhttp.send()
+	}
+	
+	function searchFunc() {
+		var name = document.getElementById("mySearch").value;
+		var xhttp = new XMLHttpRequest();
+		xhttp.onreadystatechange = function() {
+			if (xhttp.readyState == 4 && xhttp.status == 200) {
+				document.getElementById("content").innerHTML = xhttp.responseText;
+			}
+		};
+		xhttp.open("GET", "Search?searching=" + name, true)
+		xhttp.send(name)
+	}
+
 	var xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function() {
     	if (xhttp.readyState == 4 && xhttp.status == 200) {
